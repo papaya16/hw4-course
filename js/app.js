e901ee24d4df01be891acceee27dd4a23bea925e
papaya16 <papayah16@gmail.com>
2014-11-21 21:47:11 +0800

    uu

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ var loadPagesInfo = function(pages){
 
       FB.api(item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
-        $page.find('.thumbnail img').attr('src', respones.data.url)
+        $page.find('.thumbnail img').attr('src', response.data.url)
         counter++;
         $page.appendTo(current);
         // 塞完資料以後處理一下斷行
